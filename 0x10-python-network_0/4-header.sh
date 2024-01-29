57c7b31c50888348c016e8a6e286378368dc3f0a
Abeer-M-Ali <abeerragab5211@gmail.com>
2024-01-29 03:54:44 +0200

    cURL headers

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Send a get request with custom set HEADER variable
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -s "$1" -X GET -H "X-School-User-Id: 98"
+echo "Hello School!"
